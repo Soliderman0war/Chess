e31fc15a9cb4571dcd9cbb1f8e77b4ff1c21f034
Soliderman0war <drakeamueller@gmail.com>
2024-09-18 17:15:48 -0600

    fixing due to restart

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,9 +7,13 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-
+    private ChessPiece[][] board = new ChessPiece[8][8];
     public ChessBoard() {
-        
+//        ChessGame.TeamColor teamColor = ChessGame.TeamColor.WHITE;
+//        ChessPiece.PieceType pieceType = ChessPiece.PieceType.KING;
+//        ChessPosition position = new ChessPosition(0, 0);
+//        ChessPiece piece = new ChessPiece(teamColor, pieceType);
+//        addPiece(position, piece);
     }
 
     /**
@@ -19,7 +23,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
@@ -30,7 +34,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getRow()][position.getColumn()];
     }
 
     /**
