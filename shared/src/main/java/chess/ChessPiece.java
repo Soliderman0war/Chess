f24a69f049c36ef21df6e751a3e3d5ab3f04b64a
Soliderman0war <drakeamueller@gmail.com>
2024-09-19 22:20:10 -0600

    pawn

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -10,8 +10,11 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
-
+    private PieceType pieceType;
+    private ChessGame.TeamColor pieceColor;
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceType = type;
+        this.pieceColor = pieceColor;
     }
 
     /**
@@ -30,14 +33,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return this.pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return this.pieceType;
     }
 
     /**
@@ -47,7 +50,37 @@ public class ChessPiece {
      *
      * @return Collection of valid moves
      */
+   public Collection<ChessMove> PawnMoves(ChessBoard board, ChessPosition myPosition) {
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+
+        if(board.getPiece(myPosition).getTeamColor() == ChessGame.TeamColor.BLACK){
+            if(row == 6) {
+                // Check for initial position of black pawn and add two-square move
+                moves.add(new ChessMove(myPosition, new ChessPosition(row - 1, col), null));
+                moves.add(new ChessMove(myPosition, new ChessPosition(row - 2, col), null));
+            }
+            else{
+                //one-square move for black if not starting position
+                moves.add(new ChessMove(myPosition, new ChessPosition(row - 1, col), null));
+
+            }
+        }
+
+
+        if(board.getPiece(myPosition).getTeamColor() == ChessGame.TeamColor.WHITE){
+            if(row == 1) {
+                moves.add(new ChessMove(myPosition, new ChessPosition(row + 1, col), null));
+                moves.add(new ChessMove(myPosition, new ChessPosition(row + 2, col), null));
+            }
+            else{
+                moves.add(new ChessMove(myPosition, new ChessPosition(row + 1, col), null));
+            }
+        }
+        return moves;
+    }
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return new ArrayList<>();
+        return PawnMoves(board, myPosition);
     }
 }
