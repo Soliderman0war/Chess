0de9a578786fe9ef6ccc2678d696874c920468d3
Soliderman0war <drakeamueller@gmail.com>
2024-10-23 00:57:08 -0600

    Start DAO

---
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -0,0 +1,12 @@
+package dataaccess;
+
+import model.AuthData;
+
+public interface AuthDAO {
+
+    void addAuth(AuthData authData);
+
+    AuthData getAuth(String authToken);
+
+    void deleteAuth(String authToken);
+}
