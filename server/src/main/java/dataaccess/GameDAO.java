50b02e0fa639eb02844521619313765218c0637b
Soliderman0war <drakeamueller@gmail.com>
2024-10-23 10:31:20 -0600

    DAO

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,7 +1,13 @@
 package dataaccess;
 
+import model.GameData;
+
 public interface GameDAO {
+    void createGame(GameData game) throws DataAccessException;
 
     GameData getGame(int gameID) throws DataAccessException;
+
+    void updateGame(GameData game) throws DataAccessException;
+
     boolean gameExists(int gameID);
 }
