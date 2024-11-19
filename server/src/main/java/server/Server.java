10929ed6c28ac0269516bba44e770fa2badca7c0
Soliderman0war <drakeamueller@gmail.com>
2024-11-18 20:46:28 -0700

    update

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,25 @@
 package server;
 
 import spark.*;
+import dataaccess.*;
+import service.GameService;
+import service.UserService;
 
 public class Server {
+
+    UserDAO userDAO;
+    AuthDAO authDAO;
+    GameDAO gameDAO;
+
+    static UserService userService;
+    static GameService gameService;
+
+//    public Server() {
+//
+//        userService = new UserService(userDAO, authDAO);
+//        gameService = new GameService(gameDAO, authDAO);
+//
+//    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,6 +27,21 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+
+        Spark.delete("/db", this::clear);
+        Spark.post("/user", userHandler::register);
+        Spark.post("/session", userHandler::login);
+        Spark.delete("/session", userHandler::logout);
+
+        Spark.get("/game", gameHandler::listGames);
+        Spark.post("/game", gameHandler::createGame);
+        Spark.put("/game", gameHandler::joinGame);
+
+        Spark.exception(BadRequestException.class, this::badRequestExceptionHandler);
+        Spark.exception(UnauthorizedException.class, this::unauthorizedExceptionHandler);
+        Spark.exception(Exception.class, this::genericExceptionHandler);
+
+
 
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
